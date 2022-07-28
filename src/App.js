ae403476627d76ab41e3142a3cd40cc62c615d39
LEEDAESUNG <bitkorea21c@naver.com>
2022-07-29 00:33:01 +0900

    map 기능 추가

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         제목변경(copy);
       }} > 가나다순 정렬</button>
 
-      <button onClick={() => {
+      {/* <button onClick={() => {
         //let copy = 글제목; //array 또는 object 는 포인터개념, copy == 글제목
         let copy = [...글제목];
         copy[0] = '여자코드 추천';
@@ -45,19 +45,39 @@ function App() {
       <div className="list">
         <h4 onClick={()=>{ 
           if(modal === true) setModal(false) 
-          if(modal === false) setModal(true)
+          else if(modal === false) setModal(true)
            }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
 
       {/* <div className='modal'>
         <h4>제목</h4>
         <p>날짜</p>
         <p>상세내용</p>
       </div> */}
-      { 
+      
+      {
+        글제목.map(function(a,i){ //글제목 배열수 만큼 반복
+          return (
+          <div className="list" key={i} >
+              <h4 onClick={ ()=>{
+                    if(modal === true) setModal(false)
+                    else if(modal === false) setModal(true)
+                  }
+                }>
+                {글제목[i]}<span onClick={() => { 좋아요변경(좋아요+1) }}>💖</span> { 좋아요 } 
+              </h4>
+              <p>2월 17일 발행</p>
+          </div>
+          )
+        })
+      }
+
+      {
         modal === true ? <Modal></Modal> : null
       }
+
+      
 
     </div>
   );
